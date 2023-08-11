88728f3ffcd3ae5924ee717d3399f5517b54691a
Ornela <ornela.mullaymeri30@outlook.com>
2023-08-12 00:35:54 +0200

    Last update of practical project

---
+++ src/main/java/org/piccolino/utilities/Initializer.java
@@ -0,0 +1,97 @@
+package org.piccolino.utilities;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.piccolino.entities.Order;
+import org.piccolino.entities.OrderItem;
+import org.piccolino.entities.Product;
+import org.piccolino.repositories.OrderItemRepository;
+import org.piccolino.repositories.OrderRepository;
+import org.piccolino.repositories.ProductRepository;
+import org.piccolino.services.OrderService;
+import org.piccolino.services.ProductService;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+@RequiredArgsConstructor
+public class Initializer {
+    public static void init() {
+        SessionFactory sessionFactory = DbConnection.getFACTORY();
+
+        ProductRepository productRepository = new ProductRepository(sessionFactory);
+        OrderRepository orderRepository = new OrderRepository(sessionFactory);
+        OrderItemRepository orderItemRepository = new OrderItemRepository(sessionFactory);
+
+        ProductService productService = new ProductService(productRepository);
+        OrderService orderService = new OrderService(orderRepository, orderItemRepository);
+
+        productService.createProductItems();
+        System.out.println("*****************************************************************************************");
+        productService.printWelcome();
+
+//        System.out.println(productService.getProductById(1).getTitle());
+
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            productService.printMainMenu();
+            System.out.print("*         Zgjidhni veprimin qe doni te kryeni:");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 3 -> {
+                    System.out.println("*         Faleminderit qe na zgjodhet!                                                  *");
+                    System.out.println("*****************************************************************************************");
+                    return;
+                }
+                case 1 -> {
+                    Order order = new Order();
+                    while (true) {
+
+                        productService.printProductsMenu();
+                        System.out.print("*         Zgjidhni ID e produktit:");
+                        int productId = sc.nextInt();
+                        Product p;
+                        if ((p = productService.getProductById(productId)) == null) {
+                            System.out.println("*         Gabim ne sistem!                               *");
+                            return;
+                        }
+                        System.out.print("*         Vendosni sasine per produktin e zgjedhur:");
+                        int quantity = sc.nextInt();
+
+                        order.addOrderItem(p, quantity);
+
+                        System.out.println("*         Produkti u zgjodh me sukses.                                                  *");
+
+                        productService.printOrderMenu();
+                        System.out.print("*         Zgjidhni veprimin qe doni te kryeni me pas:");
+                        int option = sc.nextInt();
+                        if (option == 1) {
+                            continue;
+                        } else if (option == 2) {
+                            productService.showOrderItems(order);
+                            System.out.print("*         Vendosni id e produktit qe doni te fshini:");
+                            int idToBeDeleted = sc.nextInt();
+                            order.removeOrderItem(idToBeDeleted);
+                            System.out.println("*         Produkti u fshi me sukses!                                                    *");
+                        } else if (option == 3) {
+                            orderService.completeOrder(order);
+                            orderService.saveOrderItems(order);
+                            System.out.println("*         Porosia juaj u perfundua me sukses.                                           *");
+                            productService.printInvoice(order);
+                            break;
+                        }
+                    }
+                }
+                case 2 -> {
+                    System.out.print("*         Vendosni ID e fatures qe po kerkoni:");
+                    int orderId = sc.nextInt();
+                    Order order = orderService.getOrder(orderId);
+                    productService.printInvoice(order);
+                }
+            }
+        }
+    }
+}
